5aae7393a02954b6509b227fcd197c26e0cce1fb
William <will@penguin.com>
2024-02-19 13:59:01 -0400

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
-    puts("Programming is like building a multilingual puzzle,");
-    return 0;
+/**
+ * main - start of the program
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 }
