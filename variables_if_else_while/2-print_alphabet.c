49842396937463dc6f1429b07adcddb6e6678015
William <will@penguin.com>
2024-02-22 13:30:51 -0400

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Start of the project
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <=  'z'; i++)
+putchar(i);
+putchar('\n');
+
+return (0);
+}
