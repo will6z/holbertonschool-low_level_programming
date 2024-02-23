5f042ccf346c6f90e55a44dffa51543f8d5835db
William <will@penguin.com>
2024-02-23 09:51:17 -0400

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - start of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	for (a = 0; a <= 10; a++)
+
+		putchar (a);
+	putchar ('\n');
+
+	return (0);
+}
