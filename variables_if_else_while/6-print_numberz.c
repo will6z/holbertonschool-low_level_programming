ad30c7b4c576ced3fb3a7dbbdeeea43cd07cf488
William <will@penguin.com>
2024-02-23 10:05:04 -0400

    6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,11 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 10; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		putchar (a);
+		putchar(a);
 	}
-	{
-	putchar ('\n');
-	}
+	putchar('\n');
+
 	return (0);
 }
