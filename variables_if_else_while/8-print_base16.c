ac3d3cb077fc874f265b13f17d942b0ebec8afc3
William <will@penguin.com>
2024-02-23 11:14:04 -0400

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - start of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int i;
+
+	for (a = '0'; a >= '9'; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'a' ; a>= 'f'; a++"
+	putchar('\n');
+
+	return (0);
+}
