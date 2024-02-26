5d278f048723318caab0d676f40ea834d4499862
William <will@penguin.com>
2024-02-26 14:00:24 -0400

    0

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Start of the program
+ *
+ * Return: 0]
+ */
+int main(void) 
+	int _isdigit(int c);
+{
+	if (x <= 48 && x <=57)
+	{
+	return (1)
+	}
+	return (0)
+}
