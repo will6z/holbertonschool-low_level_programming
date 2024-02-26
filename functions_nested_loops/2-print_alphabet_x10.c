7fad9fb9911e7eda767140e02cf17e6458715c3d
William <will@penguin.com>
2024-02-26 14:26:15 -0400

    0

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,7 @@
 
 /**
  * print_alphabet_x10 - prints the alphabet x10
+ *
  */
 void print_alphabet_x10(void)
 {
---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase
+ * @c: LOL
+ * Return: 1, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
