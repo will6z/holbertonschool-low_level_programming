7d9c628da27a1997a82af9c11623d5196639aa1f
William <will@penguin.com>
2024-02-27 10:43:31 -0400

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -7,10 +7,12 @@
 void print_most_numbers(void)
 {
 int c;
+int i = '4';
+int x = '2';
 
 	for (c = '0'; c <= '9'; c++)
 
-	if (c != '2' && c != '4')
+	if (i != '2' && x != '4')
 
 		{
 		_putchar(c);
