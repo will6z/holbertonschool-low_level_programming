f92dcf8acc37fb607c3431d62f3f887e4a5f116b
William <will@penguin.com>
2024-02-28 14:12:15 -0400

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,22 +7,17 @@
 void print_diagonal(int n)
 {
 	if (n > 0)
-	{
 		_putchar('\n');
-	} else
+	else
 	{
 		int i, j;
 
-		for (i = 0; i < n; i++);
+		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < n; j++)
-		{
-			if (j == i)
-			_putchar ('\\');
-			else if (j < i)
-			_putchar (' ');
+			for (j = 0; j <= i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar('\n');
-	}
 	}
 }
