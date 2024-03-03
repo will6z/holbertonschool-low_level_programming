ca1e2ea20c1c929d2ada6792164c99492be58597
William <will@penguin.com>
2024-03-03 00:23:27 -0400

    1

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_strlen - returns the length of a string
+ *@s: string
+ *Return: 0
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
