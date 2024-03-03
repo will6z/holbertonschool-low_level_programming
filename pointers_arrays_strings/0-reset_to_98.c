791c1438096c656799980737d815496d8a14bf00
William <will@penguin.com>
2024-03-03 00:09:59 -0400

    0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *reset_to_98 - Start of the function
+ *
+ *@n: input
+ *
+ *Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+int _putchar(char c);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
