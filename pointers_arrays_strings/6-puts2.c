e3e3db398f2f98ce48a4c9fe0710070a74eb2d29
William <will@penguin.com>
2024-03-04 10:09:24 -0400

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts2 - print only one characte
+ * @str: input
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar (str[o]);
+		}
+	}
+	_putchar('\n');
+}
