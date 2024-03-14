dee2dc76247850584172fb03c3e892555e0353b4
William <will@penguin.com>
2024-03-14 10:07:13 -0400

    0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: input
+ * Return: 0
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
