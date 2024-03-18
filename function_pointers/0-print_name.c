e6a21c866fbfd8ae2f2806db2bd33de019e3763f
William <will@penguin.com>
2024-03-18 09:45:19 -0400

    0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_name - prints name
+ * @name: string to add
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
