d6bbd518c9a79613d89c4906ee3e62179b547b24
William <will@penguin.com>
2024-03-22 13:37:35 -0400

    1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ *init_dog - function
+ *@d: pointer
+ *@name: name
+ *@age: age
+ *@owner: owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+}
